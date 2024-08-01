d0d0154a7c94d2223994762cd569234b3aeb18a9
Eda Işıl İnal <edainalisil@gmail.com>
2024-08-01 12:19:24 +0300

    add css home page

--- movie/src/app/home/page.jsx
+++ movie/src/app/home/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Box } from '@mui/material';
+import { Grid, Box, Button } from '@mui/material';
 
 function Home() {
   return (
@@ -7,35 +7,63 @@ function Home() {
       container
       sx={{
         mx: 'auto',
-        maxWidth: '100%', // Ensures the grid container spans the entire width of the screen
+        maxWidth: '100%',
         height: '100vh',
         padding: 2,
+        maxWidth: "1000px",
+        margin: "0 auto",
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around'
       }}
-      alignItems="center"
-      justifyContent="center"
-      spacing={2} // Adds spacing between grid items
     >
-      {Array.from({ length: 8 }).map((_, index) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} key={index} display="flex" justifyContent="center">
-          <Box
-            height={250}
-            width="100%" // Use percentage width for responsiveness
-            maxWidth={200} // Max width for the box
-            my={4}
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            gap={4}
-            p={2}
-            sx={{
-              border: '2px solid grey',
-              backgroundColor: ['palevioletred', 'orange', 'purple', 'turquoise', 'yellowgreen', 'palevioletred', 'blueviolet', 'red'][index]
-            }}
-          >
-            {index + 1}
-          </Box>
+      <Grid item lg={2}>
+        <Box>
+          sdsdds
+        </Box>
+      </Grid>
+      <Grid item lg={10}>
+        <Grid container spacing={2}>
+          {Array.from({ length: 8 }).map((_, index) => (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              key={index}
+              sx={{
+                flexGrow: 1,
+                flexShrink: 1,
+                flexBasis: '200px', 
+              }}
+            >
+              <Box
+                height={250}
+                width="100%"
+                maxWidth={200}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                
+                p={2}
+                sx={{
+                  border: '2px solid grey',
+                  cursor:"pointer",
+                  backgroundColor: ['palevioletred', 'orange', 'purple', 'turquoise', 'yellowgreen', 'palevioletred', 'blueviolet', 'red'][index],
+                }}
+              >
+                {index + 1}
+              </Box>
+            
+            </Grid>
+          ))}
+          
         </Grid>
-      ))}
+        <Button sx={{mt:"3rem", mb:"3rem"}} variant="contained" color="secondary">
+  Load more
+</Button>
+      </Grid>
     </Grid>
   );
 }
